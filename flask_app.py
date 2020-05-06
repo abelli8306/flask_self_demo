bccb72e3a864107a9728a86b10ba1324e764350b
xiaoboli <xiaoboli@tencent.com>
2020-05-06 20:01:35 +0800

    add flask_self_demo project

--- flask_app.py
+++ flask_app.py
@@ -7,7 +7,14 @@ Author : xiaoboli
 Change Activity:
 -------------------------------------------------
 """
+import json
 
 __author__ = 'xiaoboli'
 
-from .app import create_app
+from app import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    # app.config['JSON_AS_ASCII'] = False
+    app.run(host=app.config['HOST'], debug=app.config['DEBUG'], port=app.config['PORT'])
